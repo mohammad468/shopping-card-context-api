05eeb3cd3c289b835cb6294620416088225a9018
mohammad468 <mohammad.mohseni468@gmail.com>
2024-09-22 17:01:54 +0330

    create product context

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+import { useProducts } from "../context/ProductContext";
+
 function ProductsPage() {
+  const products = useProducts();
   return <div>ProductsPage</div>;
 }
 
