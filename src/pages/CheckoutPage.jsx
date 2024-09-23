974942d7d42764466de14cb90e1dfe2ae36f71c7
mohammad468 <mohammad.mohseni468@gmail.com>
2024-09-23 17:48:59 +0330

    add empty in checkout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -2,10 +2,10 @@ import BasketCard from "../components/BasketCard";
 import { useCart } from "../context/CartContext";
 function CheckoutPage() {
   const [state, dispatch] = useCart();
-  console.log({ state, dispatch });
   return (
     <div>
       <div>
+        {state.selectedItems.length === 0 && <div>empty</div>}
         {state.selectedItems.map((product) => (
           <BasketCard key={product.id} data={product} />
         ))}
