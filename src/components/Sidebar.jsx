79bfd8c300ebbe27387899c665e9d4280b7508be
mohammad468 <mohammad.mohseni468@gmail.com>
2024-09-23 13:51:15 +0330

    add search and category

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { FaListUl } from "react-icons/fa";
+import { createQueryObject } from "../helper/helper";
+
+import { categories } from "../constants/list";
+
+import styles from "./Sidebar.module.css";
+
+function Sidebar({ query, setQuery }) {
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    setQuery((query) => createQueryObject(query, { category }));
+  };
+  return (
+    <div className={styles.sidebar}>
+      <div>
+        <FaListUl />
+        <p>Categories</p>
+      </div>
+      <ul onClick={categoryHandler}>
+        {categories.map((item) => (
+          <li key={item.id} className={query.category === item.type.toLowerCase() && styles.selected}>
+            {item.type}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,22 +1,50 @@
+import { useEffect, useState } from "react";
 import { useProducts } from "../context/ProductContext";
+import { useSearchParams } from "react-router-dom";
 
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 
 import styles from "./ProductsPage.module.css";
+import { filterProducts, getInitialQuery, searchProducts } from "../helper/helper";
+import SearchBox from "../components/SearchBox";
+import Sidebar from "../components/Sidebar";
 
 function ProductsPage() {
   const products = useProducts();
+
+  const [search, setSearch] = useState("");
+  const [displayed, setDisplayed] = useState([]);
+  const [query, setQuery] = useState({});
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setDisplayed(products);
+    setQuery(getInitialQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "");
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader/>}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <Sidebar query={query} setQuery={setQuery} />
       </div>
-      <div>sidebar</div>
-    </div>
+    </>
   );
 }
 
